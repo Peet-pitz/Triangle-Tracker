4ee56864aaf427db700bab8d8ffb9b053461c54a
Peter Njoroge <njoroge.pitz@gmail.com>
2018-08-10 10:32:45 +0300

    add code for scalene triangle

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ var sideA = parseInt(prompt("Enter side a"));
 var sideB = parseInt(prompt("Enter side b"));
 var sideC = parseInt(prompt("Enter side c"));
         if (sideA === sideB && sideB === sideC) {
-            alert ("Equilateral Triangle");
+            alert ("Equilateral Triangle")
       } else if (sideA === sideB && sideB !== sideC || sideA !== sideB && sideA === sideC || sideB === sideC && sideB !== sideA) {
-          alert("Isosceles Triangle");
+          alert("Isosceles Triangle")
+      } else if (sideA !== sideB && sideB !== sideC) {
+          alert("Scalene Triangle");
       };
