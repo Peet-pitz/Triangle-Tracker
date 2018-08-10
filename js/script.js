cd5962f9775b5a05bda5dc807c60614cda0faf70
Peter Njoroge <njoroge.pitz@gmail.com>
2018-08-10 09:28:08 +0300

    add initial triangle tracker page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var side, a = parseInt(prompt("Enter side a"));
+var side, b = parseInt(prompt("Enter side b"));
+var side, c = parseInt(prompt("Enter side c"));
+        if (side, a === side, b === side, c) {
+            alert ("Equilateral Triangle");
+        }
