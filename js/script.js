16f57f0bb2b64aeb0c64a569d8f32ee834335e7d
Peter Njoroge <njoroge.pitz@gmail.com>
2018-08-10 12:50:58 +0300

    add code for isosceles triangle

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 var sideA = parseInt(prompt("Enter side a"));
 var sideB = parseInt(prompt("Enter side b"));
 var sideC = parseInt(prompt("Enter side c"));
+ 
         if (sideA === sideB && sideB === sideC) {
             alert ("Equilateral Triangle")
       } else if (sideA === sideB && sideB !== sideC || sideA !== sideB && sideA === sideC || sideB === sideC && sideB !== sideA) {
           alert("Isosceles Triangle")
       } else if (sideA !== sideB && sideB !== sideC) {
           alert("Scalene Triangle");
+      } else if ((sideA + sideB <= sideC) || (sideA + sideC <= sideB) || (sideB + sideC <= sideA)) {
+          alert("Not a Triangle");
       };
